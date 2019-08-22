e755f696baeeb119bad9f6bfb129478c03d19533
Nicolas Ferragamo <nferragamo@est.frba.utn.edu.ar>
2019-08-22 16:14:50 -0300

    Agrege subpaquete signals y modulo senoidal

--- testbench_seno.py
+++ testbench_seno.py
@@ -5,15 +5,15 @@ Created on Wed Aug 21 19:20:28 2019
 
 @author: nico
 """
-
-# Testbench_senoidal
-
+import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-from pdsmodulos.senoidal import seno 
-import random
 
+from pdsmodulos.signals import senoidal 
+
+os.system ("clear") # limpia la terminal de python
+plt.close("all")    #cierra todos los graficos 
 
 N  = 1000 # muestras
 fs = 1000 # Hz
@@ -24,20 +24,19 @@ f0 = 10   # Hz
 w  = 2 * np.pi * f0
 
 
+#%% generacion y muestreo de senoidal
 
-
-tt, signal = seno(fs, f0, N, a0, p0)
-#plt.figure("Funcion  senoidal")
-#plt.plot(tt, signal,color='blue',label='sin(wt)')
-#plt.xlabel('tiempo [segundos]')
-#plt.ylabel('amplitud [V] ')
-#plt.axhline(0, color="black")
-#plt.axvline(0, color="black")
-#plt.grid()
-#plt.title('Funcion senoidal')
-#plt.ion()
-#plt.savefig("funciones.eps")
-#plt.legend(loc=1)
+tt, signal = senoidal.seno(fs, f0, N, a0, p0)
+ax = plt.figure("Funcion  senoidal")
+plt.plot(tt, signal,color='blue',label='sin(wt)')
+plt.xlabel('tiempo [segundos]')
+plt.ylabel('amplitud [V] ')
+plt.axhline(0, color="black")
+plt.axvline(0, color="black")
+plt.grid()
+plt.title('Funcion senoidal')
+plt.legend(loc = 'upper right')
+plt.show()
 
 #%%  TRABAJAMOS CON LA FFT
 
@@ -46,8 +45,8 @@ mod_signal = np.abs(fftsignal)
 fase_signal = np.angle(fftsignal)
 
 
+plt.figure("FFT de la señal")
 plt.subplot(2,1,1)
-plt.figure("Modulo de la señal senoidal")
 freq = np.linspace(0, (N-1)*df,N-1)/fs
 plt.plot(freq, mod_signal, color='blue', label='modulo')
 plt.xlabel('frecuecnia [Hz]')
@@ -56,8 +55,7 @@ plt.axhline(0, color="black")
 plt.axvline(0, color="black")
 plt.grid()
 plt.title('Modulo de la señal senoidal')
-plt.legend(loc=1)
-
+plt.legend(loc = 'upper center')
 
 
 plt.subplot(2,1,2)
@@ -68,4 +66,6 @@ plt.axhline(0, color="black")
 plt.axvline(0, color="black")
 plt.grid()
 plt.title('fase de la señal senoidal')
-plt.legend(loc=1)
+plt.legend(loc = 'upper center')
+plt.tight_layout() #para ajustar el tamaño de lo contrario se puperpinan los titulos
+plt.show()
