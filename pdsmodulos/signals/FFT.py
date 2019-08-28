a6896397fb93da20858517191222b17a9dea09b0
Nicolas Ferragamo <nferragamo@est.frba.utn.edu.ar>
2019-08-28 16:04:07 -0300

    Correción en my_DFT

--- pdsmodulos/signals/FFT.py
+++ pdsmodulos/signals/FFT.py
@@ -8,16 +8,16 @@ Created on Wed Aug 28 10:56:13 2019
 import os
 import matplotlib.pyplot as plt
 import numpy as np
-import math 
+import cmath 
 
 #%% Lipieza de gráficos
 
-#os.system ("clear") # limpia la terminal de python
-#plt.close("all")    #cierra todos los graficos 
+os.system ("clear") # limpia la terminal de python
+plt.close("all")    #cierra todos los graficos 
 
 #%%  TRABAJAMOS CON LA FFT
 
-def plotFFT (fftsignal, fs, N, y1l='amplitud [V] ', y2l='Fase [rad] ', p1t='title', p2t='title', tp='title', loc1='upper right', loc2='upper right') :
+def plotFFT (fftsignal, fs, N, y1l='amplitud [UA] ', y2l='Fase [rad] ', p1t=' ', p2t=' ', tp="FFT de la señal", loc1='upper right', loc2='upper right') :
     
     mod_signal = np.abs(fftsignal)
     fase_signal = np.angle(fftsignal)
@@ -25,7 +25,7 @@ def plotFFT (fftsignal, fs, N, y1l='amplitud [V] ', y2l='Fase [rad] ', p1t='titl
     
     
 #%% Ploteo de la FFT
-    plt.figure("FFT de la señal")
+    plt.figure(tp)
     plt.subplot(2,1,1)
     freq = np.linspace(0, (N-1)*df,N-1)/fs
     plt.plot(freq[0:int(N/2+1)], mod_signal[0:int(N/2+1)], color='blue', label='modulo')
@@ -34,7 +34,7 @@ def plotFFT (fftsignal, fs, N, y1l='amplitud [V] ', y2l='Fase [rad] ', p1t='titl
     plt.axhline(0, color="black")
     plt.axvline(0, color="black")
     plt.grid()
-    plt.title('Modulo de la señal'+p1t)
+    plt.title('Modulo de la señal '+p1t)
     plt.legend(loc = loc1)
 
 
@@ -45,21 +45,22 @@ def plotFFT (fftsignal, fs, N, y1l='amplitud [V] ', y2l='Fase [rad] ', p1t='titl
     plt.axhline(0, color="black")
     plt.axvline(0, color="black")
     plt.grid()
-    plt.title('fase de la señal'+p2t)
+    plt.title('fase de la señal '+p2t)
     plt.legend(loc = loc2)
     plt.tight_layout() #para ajustar el tamaño de lo contrario se puperpinan los titulos
     plt.show()
     
     return 0
 
-def myDFT (signal, N) :
+def myDFT (signal) :
     
+    N =len(signal)
     Signal = np.empty(N)
     Signal[:N-1] = np.nan
     W = [ ]
     W = np.zeros((N,N),dtype=complex)  # tengo que limpiar la memoria, el vector
-    for k in range (0, N - 1):
-        for n in range (0, N - 1):
-            W[k][n] = math.exp(-1j *2 * np.pi * k * n / N)
-    Signal = W * np.transpose(complex(signal))       
+    for k in range (0, N-1):
+        for n in range (0, N-1):
+            W[k][n] = cmath.exp(-1j * 2 * np.pi * k * n/N)
+    Signal = np.dot(W,  signal) 
     return Signal
--- testbench_seno.py
+++ testbench_seno.py
@@ -12,15 +12,16 @@ import numpy as np
 
 from pdsmodulos.signals import signals as sg 
 from pdsmodulos.signals import FFT
-os.system ("clear") # limpia la terminal de python
-plt.close("all")    #cierra todos los graficos 
+
+#os.system ("clear") # limpia la terminal de python
+#plt.close("all")    #cierra todos los graficos 
 
 N  = 1000 # muestras
 fs = 1000.0 # Hz
 df= fs / N
 a0 = 1 # Volts
 p0 = 0# radianes
-f0 = 10   # Hz
+f0 = 100   # Hz
 w  = 2 * np.pi * f0
 duty = 50
 width = 100
@@ -29,7 +30,7 @@ SNR = 10
 
 #%% generacion y muestreo de senoidal
 
-tt, signal = sg.seno(fs, f0 ,N, a0, p0)
+tt, signal = sg.square(fs, f0 ,N, a0, p0,duty)
 ax = plt.figure("Funcion  senoidal")
 plt.plot(tt, signal,color='blue',label='sin(wt)')
 plt.xlabel('tiempo [segundos]')
@@ -42,8 +43,9 @@ plt.legend(loc = 'upper right')
 plt.show()
 
 fftsignal = np.fft.fft(signal)
-FFT.plotFFT(fftsignal,fs,N)
+FFT.plotFFT(fftsignal,fs,N, tp= 'numpy FFT')
+
 #%% mi DFT
-fftsignal = FFT.myDFT(signal,N)
-FFT.plotFFT(fftsignal, fs, N, tp='my DFT')
+fftsignal2 = FFT.myDFT(signal)
+FFT.plotFFT(fftsignal2, fs, N, tp='my DFT')
 
