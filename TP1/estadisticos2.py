b625a6442b08bdde38be4803b845a9140e48c8b7
Nicolas Ferragamo <nferragamo@est.frba.utn.edu.ar>
2019-10-03 17:07:22 -0300

    Estadisticos2

--- TP1/estadisticos2.py
+++ TP1/estadisticos2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep 11 11:02:15 2019
+Created on Thu Oct  3 13:31:38 2019
 
 @author: nico
 """
@@ -30,8 +30,6 @@ M = [N, 5*N, 10*N]
 a = -0.5 * df
 b = 0.5 * df
 
-#%% Genero los vectores
-
 signal = np.zeros((N, Nexp))
 
 f1 = np.zeros((Nexp))
@@ -49,8 +47,6 @@ fftpadding5 = np.zeros((M[1], Nexp), dtype=complex)
 fftpadding10 = np.zeros((M[2], Nexp), dtype=complex)
 
 mod_signal = np.zeros((N, Nexp))
-
-
 mod_padding5 = np.zeros((M[1], Nexp))
 mod_padding10 = np.zeros((M[2], Nexp))
 
@@ -73,9 +69,9 @@ energia_frecuencia = np.zeros((Nexp))
 energia_padding5 = np.zeros((Nexp))
 energia_padding10 = np.zeros((Nexp))
 
-
 #%% generación de frecuencias aleatorias
-fa = np.random.uniform(a, b, size = (Nexp, 1)) # genera aleatorios
+fa = np.random.uniform(a, b, size = (Nexp)) # genera aleatorios
+
 plt.hist(fa, bins=20, alpha=1, edgecolor = 'black',  linewidth=1)
 plt.ylabel('frequencia')
 plt.xlabel('valores')
@@ -85,24 +81,19 @@ plt.show()
 
 #%% generación de señales
 
-for ii in range(0, Nexp) :
-     f1[ii] = f0 + fa[ii]
+f1 = f0 + fa
 
 del fa     
-     
-for ii in range(0, Nexp) :
-     tt, signal[:,ii] = sg.seno(fs, f1[ii], N, a0, p0)
- 
-     
-# probar esta forma 
-# a = np.vstack([a0 * np.sin(2*np.pi*j*tt) for j in range f1])
-     
+
+tt = np.linspace(0, (N-1)/fs, N)     
+
+signal = np.vstack(np.transpose([a0 * np.sin(2*np.pi*j*tt) for j in f1]))  
+
+del tt   
 #%% Creacion de los vectores con padding
-    
-for ii in range(0, Nexp) :
-     padding5[:,ii] = np.concatenate((signal[:,ii], auxpadding5[:,ii]), axis=0)
-     padding10[:,ii] = np.concatenate((signal[:,ii], auxpadding10[:,ii]), axis=0)
-     
+
+padding5  = np.concatenate((signal, auxpadding5),  axis=0)  
+padding10 = np.concatenate((signal, auxpadding10), axis=0)
 #%% borro las variables auxiliares
 
 del auxpadding5
@@ -110,16 +101,15 @@ del auxpadding10
      
 #%% realizo las FFTs
 
-for ii in range (0, Nexp) :
-          fftsignal[:,ii] = np.fft.fft(signal[:,ii])
-          fftpadding5[:,ii] = np.fft.fft(padding5[:,ii])
-          fftpadding10[:,ii] = np.fft.fft(padding10[:,ii])         
+fftsignal    = np.fft.fft(signal,    axis=0)
+fftpadding5  = np.fft.fft(padding5,  axis=0)
+fftpadding10 = np.fft.fft(padding10, axis=0)         
           
 #%% Obtengo los módulos de cada señal
 
 for ii in range(0, Nexp) :
-     mod_signal[:,ii] = np.abs(fftsignal[:,ii])
-     mod_padding5[:,ii] = np.abs(fftpadding5[:,ii]) 
+     mod_signal[:,ii]    = np.abs(fftsignal[:,ii])
+     mod_padding5[:,ii]  = np.abs(fftpadding5[:,ii]) 
      mod_padding10[:,ii] = np.abs(fftpadding10[:,ii]) 
                
 #%% grafico una señal con cero padding
@@ -137,126 +127,55 @@ FFT.plotFFT( fftpadding10[:,0], fs, N, y1l='Amplitud [UA] ', y2l='Fase [rad] ',
 # en primer lugar realizo la prueba de maximizar el modulo de cada señal sin padding y obtengo los índices de losmaximos para cada experimento
      
 max_signal1 = np.zeros((Nexp))
-
 mod_signal1 = np.zeros((int(N/2), Nexp))
-
 mod_signal1 = mod_signal[:int(N/2), :]
-
 max_signal1 = np.amax(mod_signal1, axis=0)
+k = np.argmax(mod_signal1, axis=0)  
 
-ii = 0
-while (ii < Nexp) : 
-     jj = 0
-     while jj < int(N/2) :
-          if (max_signal1[ii] == mod_signal[jj,ii]) :
-               k[ii] = jj
-          jj += 1
-     ii += 1
-     
 #%% prueba de los estadisticos
 # en primer lugar realizo la prueba de maximizar el modulo de cada señal con padding5 y obtengo los índices de losmaximos para cada experimento
      
 max_padding5 = np.zeros((Nexp))
-
 mod_padding5_1 = np.zeros((int(M[1]/2), Nexp))
-
 mod_padding5_1 = mod_padding5[:int(M[1]/2), :]
-
 max_padding5 = np.amax(mod_padding5_1, axis=0)
-
-ii = 0
-while (ii < Nexp) : 
-     jj = 0
-     while jj < int(M[1]/2) :
-          if (max_padding5[ii] == mod_padding5[jj,ii]) :
-               k5[ii] = jj
-          jj += 1
-     ii += 1
+k5 = np.argmax(mod_padding5_1, axis=0)     
      
-     
- #%% prueba de los estadisticos
+#%% prueba de los estadisticos
 # en primer lugar realizo la prueba de maximizar el modulo de cada señal con padding10 y obtengo los índices de los maximos para cada experimento
      
 max_padding10 = np.zeros((Nexp))
-
 mod_padding10_1 = np.zeros((int(M[2]/2), Nexp))
-
 mod_padding10_1 = mod_padding10[:int(M[2]/2), :]
-
 max_padding10 = np.amax(mod_padding10_1, axis=0)
-
-ii = 0
-while (ii < Nexp) : 
-     jj = 0
-     while jj < int(M[2]/2) :
-          if (max_padding10[ii] == mod_padding10[jj,ii]) :
-               k10[ii] = jj
-          jj += 1
-     ii += 1
-     
+k10 = np.argmax(mod_padding10_1, axis=0)
      
 #%% Calculo la energia de las señales en tiempo (valor real)
 
-ii = 0
-while (ii < Nexp) :
-     for jj in range(0,N):
-          energia_tiempo[ii] += signal[jj,ii]**2
-     ii += 1
-     
-energia_tiempo /= N
+energia_tiempo = np.sum(signal**2, axis=0) / N
 
 #%% Estimación puntual 
      
 #Calculo la energia de las señales en frecuencia (valor estimado)
+energia_frecuencia = (max_signal1**2)*2/(N**2)
 
-ii = 0
-while (ii < Nexp) :
-     energia_frecuencia[ii] = (max_signal1[ii]) **2
-     energia_frecuencia[ii] = energia_frecuencia[ii] *2/(N**2)
-     ii += 1 
-
-
-     
 # Calculo la energia de las señales en padding5(valor estimado)
+energia_padding5 = (max_padding5 **2)*2/(N**2)
 
-ii = 0
-while (ii < Nexp) :
-     energia_padding5[ii] = (max_padding5[ii]) **2
-     energia_padding5[ii] = energia_padding5[ii] *2/(N**2)
-     
-     ii += 1 
-     
 # Calculo la energia de las señales en padding10(valor estimado)
-
-ii = 0
-while (ii < Nexp) :
-     energia_padding10[ii] = (max_padding10[ii]) **2
-     energia_padding10[ii] = energia_padding10[ii] *2/(N**2)
-     ii += 1 
+energia_padding10 = (max_padding10 **2) *2/(N**2)
 
 
- 
 #%% Calculo de error
 
 # error en la señal sin cero padding
-     
-ii = 0
-while ii < Nexp :
-     error_signal[ii] = (energia_tiempo[ii] - energia_frecuencia[ii]) / energia_tiempo[ii]
-     ii += 1
+error_signal = energia_tiempo - energia_frecuencia
 
 # error en la señal con cero padding5
-     
-ii = 0
-while ii < Nexp :
-     error_padding5[ii] = (energia_tiempo[ii] - energia_padding5[ii]) / energia_tiempo[ii]
-     ii += 1
+error_padding5 = energia_tiempo - energia_padding5
      
 # error en la señal con cero padding10
-ii = 0
-while ii < Nexp :
-     error_padding10[ii] = (energia_tiempo[ii] - energia_padding10[ii]) / energia_tiempo[ii] 
-     ii += 1
+error_padding10 = energia_tiempo - energia_padding10
 
 #%% Ploteo de los errores al estimar la enrgia 
 
@@ -283,22 +202,17 @@ plt.xlim((249.5,250.5))
 plt.grid()
 plt.title('Errores relativos de estimación espectral')
 plt.legend(loc = 'upper right')
-plt.savefig("Err_relativos.png")
-     
-     
-     
+
      
 #%% error en la frecuencia
-
 
 error_fsignal = np.zeros((Nexp))
 error_fpadding5 = np.zeros((Nexp))
 error_fpadding10 = np.zeros((Nexp))
  
-
-error_fsignal    = abs((k - f1) )
-error_fpadding5  = abs((k5 / 5 - f1) )
-error_fpadding10 = abs((k10 / 10 - f1) )
+error_fsignal    = abs(k - f1)
+error_fpadding5  = abs(k5 / 5 - f1) 
+error_fpadding10 = abs(k10 / 10 - f1) 
 
 #%% Cálculos estadisticos
 
@@ -334,20 +248,18 @@ dstd_error_fpadding10= stats.stdev(error_fpadding10)
 print("Desviación estandar de error_fpadding10: ",dstd_error_fpadding10,"\n")
 
 
-
 #%% Ploteo de los errores
 
 plt.figure("Histograma de errores al estimar la frecuencia")
-plt.hist(error_signal, bins=20, alpha=1, edgecolor = 'black',  linewidth=1, label="signal")
-plt.hist(error_padding5, bins=20, alpha=1, edgecolor = 'black',  linewidth=1, label="padding5")
-plt.hist(error_padding10, bins=20, alpha=1, edgecolor = 'black',  linewidth=1, label="padding10")
+plt.hist(error_fsignal, bins=20, alpha=1, edgecolor = 'black',  linewidth=1, label="signal")
+plt.hist(error_fpadding5, bins=20, alpha=1, edgecolor = 'black',  linewidth=1, label="padding5")
+plt.hist(error_fpadding10, bins=20, alpha=1, edgecolor = 'black',  linewidth=1, label="padding10")
 plt.legend(loc = 'upper right')
 plt.ylabel('frecuencia')
 plt.xlabel('valores')
 plt.title('histograma de errores al estimar la frecuencia')
 plt.show()
-plt.savefig("Histograma_err_frec.png")
-plt.savefig("Histograma_err_frec.eps", format='eps')
+
 
 #%% Ploteo de los errores
 plt.figure("Grafico de errores al estimar la frecuencia")
@@ -360,5 +272,3 @@ plt.ylabel('error relativo')
 plt.xlabel('frecuencias')
 plt.title("Grafico de errores al estimar la frecuencia")
 plt.show()
-plt.savefig("grafico_err_frec.png")
-plt.savefig("grafico_err_frec.eps",format='eps')
