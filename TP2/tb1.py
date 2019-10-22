66b6b44e96d40612d9a5c1e5f996807f4f50d4d9
Nicolas Ferragamo <nferragamo@est.frba.utn.edu.ar>
2019-10-22 20:08:20 -0300

    TP2 2b completado

--- TP2/tb1.py
+++ TP2/tb1.py
@@ -28,9 +28,9 @@ f0 = fs / 4
 
 M = 9*N
 
-ventanas = [np.bartlett(N), wds.triang(N), np.hanning(N), wds.hann(N),    np.blackman(N), wds.blackamanHarris(N), sg.flattop(N), wds.flattop(N)]
+ventanas = [np.bartlett(N), wds.triang(N), np.hanning(N), wds.hann(N),    np.blackman(N), wds.blackamanHarris(N), sg.flattop(N), wds.flattop(N), sg.boxcar(N), sg.boxcar(N)]
 V =  len(ventanas)
-ventana = ["Bartell", "my_Triang", "Hanning", "my_Hanning", "Blackman", "my_Blackman", "Flattop", "my_Flattop"]
+ventana = ["Bartell", "my_Triang", "Hanning", "my_Hanning", "Blackman", "my_Blackman", "Flattop", "my_Flattop", "Rectangular", "my_Rectangular"]
 
 aux_padding = np.zeros(M, float)
 
@@ -40,7 +40,7 @@ mod_signal = np.vstack(np.transpose([np.abs(np.fft.fft(signal[:,ii]))*2/N  for i
 
 # creo las ventanas con zero padding y obtengo el móduo de su dft
 signal_padding = np.vstack(np.transpose([np.concatenate((this_w,aux_padding), axis=0)  for this_w in ventanas])) 
-mod_signal_padding = np.vstack(np.transpose([np.abs(np.fft.fft(signal_padding[:,ii]))*2/N  for ii in      range(V)]))
+mod_signal_padding = np.vstack(np.transpose([np.abs(np.fft.fft(signal_padding[:,ii]))*2/(N + M)  for ii in      range(V)]))
 
 # la paso en dB
 mod_signal = 20 *np.log10(mod_signal/mod_signal[0])
@@ -50,8 +50,9 @@ mod_signal_padding = 20 *np.log10(mod_signal_padding/mod_signal_padding[0])
 #Genero los ejes de tiempo y frecuencia
 tt = np.linspace(0, (N-1)/fs, N)
 freq = np.linspace(0, (N-1)*df, N) / fs
+freq2 = np.linspace(0, (N + M-1)*df, N + M) / (10*fs)
 
-for ii in (0,2,4,6):
+for ii in (0,2,4,6,8):
 
 #grafico de las ventanas
      fig = plt.figure("ventana " + ventana[ii], constrained_layout=True)
@@ -80,12 +81,13 @@ for ii in (0,2,4,6):
 #grafico del modulo la ventana con zero padding de python
      f_ax3 = fig.add_subplot(gs[0, 2])
      f_ax3.set_title("Zero padding " + ventana[ii])
-     f_ax3.plot(freq[0:int(N/2)], mod_signal_padding[0:int(N/2),ii], marker='.', linestyle='None')
+     f_ax3.plot(freq2[0:int((N+M)/2)], mod_signal_padding[0:int((N + M)/2),ii], marker='.', linestyle='None')
      f_ax3.set_xlabel('frecuecnia normalizada f/fs [Hz]')
      f_ax3.set_ylabel("Magnitud [dB]")
      f_ax3.axhline(0, color="black")
      f_ax3.axvline(0, color="black")
-     f_ax3.set_xlim(0, 0.12)
+     f_ax3.set_xlim(0, 0.015)
+     f_ax3.set_ylim(-150, 0)
      f_ax3.grid()
 
 #grafico de la ventana propia
@@ -111,10 +113,27 @@ for ii in (0,2,4,6):
 #grafico del modulo la ventana con zero padding propia
      f_ax6 = fig.add_subplot(gs[1, 2])
      f_ax6.set_title("Zero padding " + ventana[ii + 1])
-     f_ax6.plot(freq[0:int(N/2)], mod_signal_padding[0:int(N/2),ii + 1], marker='.', linestyle='None')
+     f_ax6.plot(freq2[0:int((N+M)/2)], mod_signal_padding[0:int((N + M)/2),ii + 1], marker='.', linestyle='None')
      f_ax6.set_xlabel('frecuecnia normalizada f/fs [Hz]')
      f_ax6.set_ylabel("Magnitud [dB]")
      f_ax6.axhline(0, color="black")
      f_ax6.axvline(0, color="black")
-     f_ax6.set_xlim(0, 0.12)
+     f_ax6.set_xlim(0, 0.015)
+     f_ax6.set_ylim(-150, 0)
      f_ax6.grid()
+
+
+fig2 = plt.figure("Comparación de los módulos de las ventanas")
+plt.plot(freq2[0:int((N + M)/2)], mod_signal_padding[0:int((N + M)/2),0], marker='.', linestyle='None', label=ventana[0])
+plt.plot(freq2[0:int((N + M)/2)], mod_signal_padding[0:int((N + M)/2),2], marker='.', linestyle='None', label=ventana[2])
+plt.plot(freq2[0:int((N + M)/2)], mod_signal_padding[0:int((N + M)/2),4], marker='.', linestyle='None', label=ventana[4])
+plt.plot(freq2[0:int((N + M)/2)], mod_signal_padding[0:int((N + M)/2),6], marker='.', linestyle='None', label=ventana[6])
+plt.plot(freq2[0:int((N + M)/2)], mod_signal_padding[0:int((N + M)/2),8], marker='.', linestyle='None', label=ventana[8])
+plt.xlabel('frecuecnia normalizada f/fs [Hz]')
+plt.ylabel("Magnitud [dB]")
+plt.axhline(0, color="black")
+plt.axvline(0, color="black")
+plt.xlim(0, 0.01)
+plt.ylim(-150, 0)
+plt.grid()
+plt.legend(loc ='upper right')
---
+++ TP2/tb2.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 22 16:35:33 2019
+
+@author: nico
+"""
+
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.fftpack import fft
+from pdsmodulos.signals import FFT
+from pdsmodulos.signals import windows as wds
+from pdsmodulos.signals import signals as sig
+
+#import seaborn as sns
+
+os.system ("clear") # limpia la terminal de python
+plt.close("all")    #cierra todos los graficos 
+
+N  = 1000 # muestras
+fs = 2*np.pi # Hz
+df = fs / N
+a0 = 1 # Volts
+p0 = 0 # radianes
+f0 = np.pi / 2
+dif= -40  #Diferencia en dB de Amplitude
+a1 = a0 * 10**(dif/20)
+f1 = f0 + 10 * df
+
+#%% Señales ej 2a
+tt = np.linspace(0, (N-1)/fs, N)  
+freq = np.linspace(0, (N-1)*df, N) / fs
+
+signal = a0 * np.sin(2*np.pi*f0*tt) + a1 * np.sin(2*np.pi*f1*tt)
+mod_signal=np.abs(np.fft.fft(signal))*2/N
+mod_signal = 20 *np.log10(mod_signal)
+
+# grafico
+fig = plt.figure("Señal bitonal " , constrained_layout=True)
+plt.title("Señal bitonal separada 10df")
+plt.plot(freq[0:int(N/2)],mod_signal[0:int(N/2)])
+plt.xlabel("Frecuencia normalizada [f/fs]")
+plt.ylabel("Magnitud [dB]")
+plt.axhline(0, color="black")
+plt.axvline(0, color="black")
+plt.grid()
+
+
+#%% Señales ej 2b
+a1 = a0 * 10**(-265/20)
+signal2 = a0 * np.sin(2*np.pi*f0*tt) + a1 * np.sin(2*np.pi*f1*tt)
+mod_signal2 = np.abs(np.fft.fft(signal2))*2/N
+mod_signal2 = 20 *np.log10(mod_signal2)
+
+# grafico
+fig = plt.figure("Señal bitonal con a1 minimo " , constrained_layout=True)
+plt.title("Señal bitonal separada 10df")
+plt.plot(freq[0:int(N/2)],mod_signal2[0:int(N/2)])
+plt.xlabel("Frecuencia normalizada [f/fs]")
+plt.ylabel("Magnitud [dB]")
+plt.axhline(0, color="black")
+plt.axvline(0, color="black")
+plt.grid()
+
+
+
+#%% Genero ruido de cuantificacion 
+a1 = a0 * 10**(-75/20)
+signal3 = a0 * np.sin(2*np.pi*f0*tt) + a1 * np.sin(2*np.pi*f1*tt)
+n = 16 # bits de cuatizacion
+qsignal3 = np.round(signal3*(2**n)/2) / ((2**n)/2)
+    
+e16 = signal3 - qsignal3
+
+mod_signal3 = np.abs(fft(qsignal3))*2/N # FFT de la señal cuantificada
+mod_signal3 = 20 *np.log10(mod_signal3)
+   
+fig = plt.figure("Señal bitonal cuantificada ")
+plt.title("Señal bitonal separada 10df")
+plt.plot(freq[0:int(N/2)],mod_signal3[0:int(N/2)])
+plt.xlabel("Frecuencia normalizada [f/fs]")
+plt.ylabel("Magnitud [dB]")
+plt.axhline(0, color="black")
+plt.axvline(0, color="black")
+plt.grid()
+
+
