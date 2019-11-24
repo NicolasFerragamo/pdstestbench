6c6a8dbfc523c5091012567a655f8dd7a61feec9
Nicolas Ferragamo <nferragamo@est.frba.utn.edu.ar>
2019-11-23 22:00:41 -0300

    TP2 y TP3 corregidos

--- TP2/tb2.py
+++ TP2/tb2.py
@@ -67,16 +67,34 @@ plt.grid()
 
 
 #%% Genero ruido de cuantificacion 
-a1 = a0 * 10**(-75/20)
+a1 = a0 * 10**(-90/20)
 signal3 = a0 * np.sin(2*np.pi*f0*tt) + a1 * np.sin(2*np.pi*f1*tt)
+# para que se vea mejor el ruido ya que es muy deterministico agregarle  ruido con distribuzión
+#uniforme de -/- 5 LSB si lo hago despues del cuentificador de -5 a +5
 n = 16 # bits de cuatizacion
+LSB = a0/2**n
+ruido = np.random.uniform(-5*LSB, 5*LSB, N)
+signal3 = signal3 + ruido
+
 qsignal3 = np.round(signal3*(2**n)/2) / ((2**n)/2)
     
 e16 = signal3 - qsignal3
 
 mod_signal3 = np.abs(fft(qsignal3))*2/N # FFT de la señal cuantificada
-mod_signal3 = 20 *np.log10(mod_signal3)
-   
+mod_signal3 = 20 *np.log10(mod_signal3 + np.finfo(float).eps)
+ 
+#tt = np.linspace(0,(N-1)/fs, N)  
+#fig = plt.figure("Señal bitonal cuantificada en tiempo ")
+#plt.title("Señal bitonal separada 10df")
+#plt.plot(tt[:20], qsignal3[:20])
+#plt.xlabel("Tiempo")
+#plt.ylabel("Amplitud")
+#plt.axhline(0, color="black")
+#plt.axvline(0, color="black")
+#plt.grid()
+
+
+
 fig = plt.figure("Señal bitonal cuantificada ")
 plt.title("Señal bitonal separada 10df")
 plt.plot(freq[0:int(N/2)],mod_signal3[0:int(N/2)])
